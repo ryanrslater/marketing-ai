c3380f6b97077f7d4dac3587a63e4da4b29da34b
Ryan Slater <78631176+ryanrslater@users.noreply.github.com>
2023-02-20 21:35:51 +0000

    SEO

--- lib/Providers/SEOInterface.ts
+++ lib/Providers/SEOInterface.ts
@@ -1,6 +1,7 @@
 import { SEOPageTypes, SEOPageKeywords, SEOPageLocations } from "@prisma/client";
 
 export interface SEOInterface {
+    create(arg0: { data: { seoPages: { create: { type: { connectOrCreate: { where: { name: string; }; create: { name: string; display: string; }; }; }; }; }; }; }): unknown;
     getHTML(url: string): Promise<string>;
     createAudit(url: string, pageType: SEOPageTypes, keywords: SEOPageKeywords[], locations: SEOPageLocations[]): Promise<string>;
     createBlogPost(keywords: SEOPageKeywords[], locations: SEOPageLocations[]): Promise<string>;
--- lib/Repositories/SEORepository.ts
+++ lib/Repositories/SEORepository.ts
@@ -1,9 +1,81 @@
-import { SEOInterface } from "../Providers/SEOInterface";
+import { PrismaClient } from "@prisma/client";
 
-export class SEORepository {
-    private seo: SEOInterface;
+
+export class SEORepository extends PrismaClient {
+    private seoProvider: SEOInterface;
 
     constructor(seo: SEOInterface) {
-        this.seo = seo;
+        super();
+        this.seoProvider = seo;
+    }
+
+    async createNewSEO() {
+        const seo = await this.sEO.create({
+            data: {
+                seoPages: {
+                    create: {
+                        type: {
+                            connectOrCreate: {
+                                where: {
+                                    name: "Home"
+                                },
+                                create: {
+                                    name: "Home",
+                                    display: "Home"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        return seo;
+    }
+
+    async createAudit(sub: string, SeoId: string): Promise<string> {
+        const user = await this.users.findUnique({
+            where: {
+                sub,
+                seoId: SeoId,
+            },
+            include: {
+                accounts: {
+                    include: {
+                        seo: {
+                            include: {
+                                seoPages: {
+                                    include: {
+                                        keywords: true,
+                                        locations: true,
+                                        type: true,
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        });
+
+        if (!user) throw new Error("User not found");
+
+        const admin = await this.users.findUnique({
+            where: {
+                sub: user?.accounts[0].owner
+            },
+            include: {
+                tier: true
+            }
+        });
+
+        if (!admin) throw new Error("Admin not found");
+
+        if (admin.tier.name === "Free" && user.accounts[0].seo.seoPages.length > 1) throw new Error("You have reached your limit of 1 SEO page");
+
+
+
+        const audit = await this.seo.createAudit(`${user.accounts[0].domain}${user.accounts[0].seo[0].seoPages[0].path}`, pageType, keywords, locations);
+        return audit;
     }
 }
